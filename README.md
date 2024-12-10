4477fdf6a3310e5216ed14d1ba79469697e347b9
Andrew Show <andrew_show@hotmail.com>
2024-12-10 22:17:37 +0800

    Add README and LICENSE

---
+++ README.md
@@ -0,0 +1,8 @@
+# pystdcxx
+
+Python wrapper of C++ container
+
+## Make and install
+
+pip install pystdcxx
+
--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from setuptools import setup, Extension
 
 
 setup(name="pystdcxx",
+      description="Python wrapper for C++ containers",
       version='0.6.0',
+      author="Andrew Chow",
+      author_email="andrew_show@hotmail.com",
+      url="https://github.com/andrew-show/pystdcxx",
       ext_modules=[
           Extension("stdcxx",
                     [ "pystdcxx.cpp", "set.cpp", "map.cpp" ],
